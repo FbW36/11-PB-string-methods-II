49220f4c8fbfb62ec795df3e44f13366c3e9fd3e
Muhasin <mahmudmuhasin@gmail.com>
2020-10-08 16:04:45 +0200

    initial commit

---
+++ muhasin/index.js
@@ -0,0 +1,15 @@
+let str = "it's hard to say who is right in this matter"
+// Find the first occurrence of the letter "a" in a string, starting the search at position 17.
+console.log("str ==>", str.indexOf("a")); // 6
+console.log("str ==>", str[17]); // w
+
+// 1. Check if a string includes "righT".
+console.log("str ==>", str.includes("righT")); // false
+
+// 2. Extract 'who' word from the text.
+
+console.log("str ===>", str.indexOf("who")); // 17
+
+// 3. Extract only the last character.
+
+console.log("str ==>", str[str.length - 1]); //  r
