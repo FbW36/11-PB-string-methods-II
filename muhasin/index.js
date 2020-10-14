5a205d98d10383e8d6edb4252d1efa5feaf77d93
Muhasin <mahmudmuhasin@gmail.com>
2020-10-14 15:28:12 +0200

    solved

--- muhasin/index.js
+++ muhasin/index.js
@@ -1,7 +1,7 @@
 let str = "it's hard to say who is right in this matter"
 // Find the first occurrence of the letter "a" in a string, starting the search at position 17.
-console.log("str ==>", str.indexOf("a")); // 6
-console.log("str ==>", str[17]); // w
+
+console.log("str ==>", str.indexOf("a", 17)); // 39
 
 // 1. Check if a string includes "righT".
 console.log("str ==>", str.includes("righT")); // false
