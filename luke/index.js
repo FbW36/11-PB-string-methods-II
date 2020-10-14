b80f9d0ca7f23c4fa029616438b8f73bf271fb73
Luke Dowling <lpdowling26@googlemail.com>
2020-10-14 11:02:42 +0200

    added changes

--- luke/index.js
+++ luke/index.js
@@ -2,7 +2,7 @@
 let argument = `It's hard to say who is right in this matter`;
 
 // 0.  Find the first occurrence of the letter "a" in a string, starting the search at position 17.
-console.log(argument.slice(17).indexOf(`a`));
+console.log(argument.indexOf(`a`, 17));
 
 // 1. Check if a string includes "righT".
 console.log(argument.includes(`righT`));
